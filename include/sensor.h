eb9b6ef501184247fa833ac2b47e12b0ffcd77fd
moenguyenx <qminh1908@gmail.com>
2024-07-24 21:08:42 +0700

    Adding Sensor class

--- include/sensor.h
+++ include/sensor.h
@@ -29,7 +29,43 @@
  *                                                  FUNCTION PROTOTYPES
 ======================================================================================================================*/
 
-/**
- * @brief 
- * 
- */
+class HeartRateSensor 
+{
+    public:
+        HeartRateSensor();
+        /**
+         * @brief Initialize MAX3010x Sensor
+         * 
+         */
+        void begin();
+
+        /**
+         * @brief Get Constant Update of BPM and SPO2
+         * 
+         */
+        void update();
+
+        /**
+         * @brief Get the Beats Per Minute object
+         * 
+         * @return Float
+         */
+        float getBeatsPerMinute() const;
+
+        /**
+         * @brief Get the Beat Avg object
+         * 
+         * @return int 
+         */
+        int getBeatAvg() const;
+
+    private:
+        MAX30105 particleSensor;
+        const byte RATE_SIZE = 4; // Increase this for more averaging. 4 is good.
+        byte rates[4]; // Array of heart rates
+        byte rateSpot = 0;
+        long lastBeat = 0; // Time at which the last beat occurred
+
+        float beatsPerMinute;
+        int beatAvg;
+};
